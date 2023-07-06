19cedffc561b00e37c55114d912d6fee92e0c75a
Geo-W <geo.wgy@gmail.com>
2023-07-06 18:32:42 +0800

    change relation_func from func trait object to function pointer

--- src/structs/querybuilder.rs
+++ src/structs/querybuilder.rs
@@ -11,21 +11,27 @@ pub struct QueryBuilder {
     pub(crate) fields: HashMap<&'static str, Vec<&'static str>>,
     pub(crate) filters: Vec<String>,
     pub(crate) join: String,
+    tables: Vec<&'static str>,
+    pub query_result: HashMap<&'static str, Vec<Value>>,
     sql: String,
-    relation_func: Box<dyn Fn(&str) -> &'static str>,
+    // relation_func: Box<dyn Fn(&str) -> &'static str>,
+    relation_func: fn(&str) -> &'static str,
+
     from_row_funcs: HashMap<String, Box<dyn Fn(&tiberius::Row) -> Map<String, Value>>>, // mapper from table name to select row func
 }
 
 impl QueryBuilder {
-    pub fn new(table: &'static str, fields: (&'static str, Vec<&'static str>), func: Box<dyn Fn(&str) -> &'static str>, from_row: Box<dyn Fn(&tiberius::Row) -> Map<String, Value>>) -> Self {
+    pub fn new(table: &'static str, fields: (&'static str, Vec<&'static str>), func: fn(&str) -> &'static str, from_row: Box<dyn Fn(&tiberius::Row) -> Map<String, Value>>) -> Self {
         QueryBuilder {
             table: table,
             fields: HashMap::from([fields]),
             filters: vec![],
+            tables: vec![table],
             join: String::new(),
             relation_func: func,
             sql: "".to_string(),
             from_row_funcs: HashMap::from([(table.to_string(), from_row)]),
+            query_result: HashMap::from([(table, vec![])]),
         }
     }
 
@@ -34,8 +40,8 @@ impl QueryBuilder {
         self
     }
 
-    pub fn join<T>(mut self) -> Self
-        where T: RusqlMarker {
+    pub fn join<T>(mut self) -> QueryBuilder
+        where T: RusqlMarker + 'static {
         let name = T::table_name();
         let fields = T::fields();
         println!("name: {:?}", name);
@@ -43,7 +49,11 @@ impl QueryBuilder {
         self.join.push_str(&format!(" LEFT JOIN {} ", relation));
         match self.fields.insert(&name, fields) {
             Some(_v) => panic!("table already joined."),
-            None => {}
+            None => {
+                self.tables.push(name);
+                self.query_result.insert(name, vec![]);
+                self.from_row_funcs.insert(name.to_string(), Box::new(T::from_row));
+            }
         }
         self
     }
@@ -57,29 +67,37 @@ impl QueryBuilder {
         self
     }
 
-    pub async fn find_all(&self, mut conn: tiberius::Client<Compat<TcpStream>>) -> Vec<Value> {
-        let mut sql = self.fields.iter()
+    pub async fn find_all(&mut self, mut conn: tiberius::Client<Compat<TcpStream>>) {
+        let sql = self.fields.iter()
             .map(|(table, fields)|
                 fields.iter().map(|field| format!(r#"{}.{} as "{}.{}""#, table, field, table, field))
                     .reduce(|cur, nxt| format!("{},{}", cur, nxt)).unwrap()
             )
             .reduce(|cur, nxt| format!("{},{}", cur, nxt)).unwrap();
+
         // let mut stream = conn.simple_query(r#"SELECT ship_to_id as "CUSTOMER_LIST.ship_to_id", ship_to as "CUSTOMER_LIST.ship_to",
         // volume as "CUSTOMER_LIST.volume", container as "CUSTOMER_LIST.container" FROM CUSTOMER_LIST"#).await.unwrap();
         dbg!(format!("SELECT {} FROM {} {} ", sql, self.table, self.join));
         let mut stream = conn.simple_query(format!("SELECT {} FROM {} {} ", sql, self.table, self.join)).await.unwrap();
-        let mut ret = Vec::new();
         while let Some(item) = stream.try_next().await.unwrap() {
             match item {
                 QueryItem::Row(row) => {
                     dbg!(&row);
-                    ret.push((self.from_row_funcs.get(self.table).unwrap())(&row).into());
+                    self.tables.iter().for_each(|table|{
+                        let vec = self.query_result.get_mut(table).unwrap().push(
+                            (self.from_row_funcs.get(&table.to_string()).unwrap())(&row).into() );
+                    });
+                    // ret.push((self.from_row_funcs.get(self.table).unwrap())(&row).into());
                 }
                 QueryItem::Metadata(_) => {}
             }
         }
-        ret
     }
+
+    pub fn get<T:RusqlMarker>(&mut self) -> Vec<Value> {
+        self.query_result.remove(T::table_name()).unwrap()
+    }
+
 
     pub async fn get_client() -> Client<Compat<TcpStream>> {
         let mut config = Config::new();
@@ -94,7 +112,7 @@ impl QueryBuilder {
         client
     }
 
-    pub async fn find_all_test(&self) -> Vec<Value> {
+    pub async fn find_all_test(&mut self) {
         let client = QueryBuilder::get_client().await;
         self.find_all(client).await
     }
@@ -104,6 +122,7 @@ impl QueryBuilder {
 pub trait RusqlMarker: Sized {
     fn table_name() -> &'static str;
     fn fields() -> Vec<&'static str>;
+    fn from_row(row:&tiberius::Row) -> Map<String, Value>;
 }
 
 struct TableInfo<RusqlMarker> {
